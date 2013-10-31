b6be1b9aa8b7708f2af7e0134b4304fae8de5aee
Aftab Alam <aftabbuddy@gmail.com>
2013-10-31 12:31:13 +0530

    first commit, humble start...:)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mongoapiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
